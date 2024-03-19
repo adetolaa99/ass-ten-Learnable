a647c1d4a265afd3cec982a9a8421bd96b7563aa
Nofisat Hassan <hassannofisat017@gmail.com>
2024-03-19 23:19:04 +0100

    added changes

--- database.js
+++ database.js
@@ -3,16 +3,11 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://adetolaaa:nafeesaH0!@cluster0.hk8gamz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      "mongodb+srv://adetolaaa:nafeesaH0!@cluster0.hk8gamz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("MongoDB Connected...");
   } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+    console.error("MongoDB Connection Error:", err.message);
   }
 };
 
